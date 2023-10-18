ea5cb14aa53c0fd527da57c9be4283c390151677
TechSergio <smarquez15@gmail.com>
2023-10-18 19:06:51 -0300

    Agrega carpeta style a Pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import SearchBar from '../Components/SearchBar'
 import Categories from '../Components/Categories'
+
+import style from './Style/Home.module.css'
+
 const Home = () => {
   return (
     <div>
