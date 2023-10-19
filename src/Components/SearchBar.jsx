4e439afd8ac188fe993f67163c7c223bad0418f1
TechSergio <smarquez15@gmail.com>
2023-10-19 03:41:00 -0300

    Se crea la funcionalidad del searchbar y de un filtro

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import style from './Styles/SearchBar.module.css'
+
 const SearchBar = () => {
+
+  const [term, setTerm] = useState('')
+  const [filter, setFilter] = useState('all')
+
+  //Manejador de cambios en la barra busqueda
+  const handleInput = ( e ) => {
+    setTerm(e.target.value)
+  }
+
+  const handleFilter = ( e ) => {
+    setFilter(e.target.value)
+  }
+
   return (
-    <div className={style.container}>SearchBar</div>
+    <div className={style.container}>
+      <input
+        type='text'
+        placeholder='Busca instrumentos...'
+        value={term}
+        onChange={handleInput}
+        />
+      <select value={filter} onChange={handleFilter}>
+        <option value="all">Todos</option>
+        <option value="Guitarra">Guitarra</option>
+        <option value="Bateria">Bateria</option>
+        <option value="Piano">Piano</option>
+      </select>
+    </div>
   )
 }
 
