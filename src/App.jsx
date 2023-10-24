b9856a666753df9c5ca6404b1165989194effc93
Ramiro Encalada <ramiroencalada@gmail.com>
2023-10-23 22:07:45 -0300

    renderizado de cards en recomendaciones

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/admin" element={<Admin />}/>
-    {/*<Route path="/Detalle/:id" element={<Detail/>}/>*/}
      <Route path="/Detail/:id" element={<Detail/>}/>
     </Routes>
 
