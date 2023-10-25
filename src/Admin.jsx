0557e31a1b900d79ba377226b45afa7be53fde9f
TechSergio <smarquez15@gmail.com>
2023-10-25 04:48:39 -0300

    Agrega advertencia en admin si se ingresa de movil, se configurarchivo list.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import List from './Pages/List.jsx';
-import AddProduct from './Pages/AddProduct.jsx';
-import { Route, Routes, Link } from 'react-router-dom';
-import HomeAdmin from './Pages/HomeAdmin.jsx';
-import Header from './Components/Header.jsx';
-import Footer from './Components/Footer.jsx';
+
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Admin = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    setIsMobile(/Mobi|Android/i.test(userAgent));
+  }, []);
+
   return (
     <>
+      {isMobile ? (
+        <div style={{ background: 'yellow', padding: '10px', marginBottom: '10px' }}>
+          ¡Advertencia! Estás accediendo desde un dispositivo móvil.
+        </div>
+      ) : (
+      <div>
       <h1>Panel de administrador</h1>
 
       <Link to='/admin/list'><button>Lista de productos</button></Link>
       <Link to='/admin/addProduct'><button>Agregar productos</button></Link>
       <p>Para hacer espacio...</p>
-
+      </div>
+      )}
     </>
   );
 };
--- src/Pages/List.jsx
+++ src/Pages/List.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import styles from './Style/List.module.css';
 
 const List = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      /* ----------------------------- Api a modificar ---------------------------- */
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setItems(response.data);
+    } catch (error) {
+      console.error('Se produjo el error:', error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    if (window.confirm('¿Confirma que desea eliminar el producto?')) {
+      try {
+
+              /* ----------------------------- Api a modificar ---------------------------- */
+        await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
+
+        /* -------------------- Busca y elimina el id de la lista ------------------- */
+        const updatedItems = items.filter(item => item.id !== id);
+        setItems(updatedItems);
+      } catch (error) {
+        console.error('Error al eliminar el elemento:', error);
+      }
+    }
+  };
+
   return (
     <div>
-        <p>asdasdasd</p>
+      <h2>Lista de productos</h2>
+      <Link to="/admin" className={styles.backButton}>
+        Volver
+      </Link>
+      <div className={styles.tableContainer}>
+        <div className={`${styles.tableRow} ${styles.header}`}>
+          <div className={styles.tableColumnId}>ID</div>
+          <div className={styles.tableColumnName}>Nombre</div>
+          <div className={styles.tableColumnActions}>Acciones</div>
+        </div>
+        {items.map((item) => (
+          <div className={styles.tableRow} key={item.id}>
+            <div className={styles.tableColumnId}>{item.id}</div>
+            <div className={styles.tableColumnName}>{item.title}</div>
+            <div className={styles.tableColumnActions}>
+              <button className={styles.deleteButton} onClick={() => handleDelete(item.id)}>
+                Eliminar
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
