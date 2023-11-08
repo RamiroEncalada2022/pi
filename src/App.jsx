94dcc1516ccad1b37529b243120efcbfcd2f01b4
Ramiro Encalada <ramiroencalada@gmail.com>
2023-11-08 09:12:14 -0300

    Merge main-ramiro2

--- src/App.jsx
+++ src/App.jsx
@@ -26,46 +26,10 @@ function App() {
       <Route path="admin" element={<Admin/>} />
       <Route path="/admin/list" element={<List />} />
       <Route path="/admin/addProduct" element={<AddProduct />} />
-      <Route path="/Detail/:id" element={<Detail/>} />
-      <Route path="/ListaInstrumentos" element={<ProductList />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-
-    <Footer/>
-      
-    </>
-  )
-}
-
-
-import './App.css'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
-import Home from './Pages/Home'
-import Detail from "./Components/Detail"
-
-import { Route, Routes } from 'react-router-dom'
-import Admin from './Admin.jsx'
-import List from './Pages/List'
-import AddProduct from './Pages/AddProduct.jsx'
-import ProductList from "./Components/ProductList"
-import AddCategory from './Pages/AddCategory.jsx'
-
-function App() {
-
-
-  return (
-    <>
-    <Header/>
-
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="admin" element={<Admin/>} />
-      <Route path="/admin/list" element={<List />} />
-      <Route path="/admin/addProduct" element={<AddProduct />} />
       <Route path="/admin/addCategory" element={<AddCategory />} />
       <Route path="/Detail/:id" element={<Detail/>} />
       <Route path="/ListaInstrumentos" element={<ProductList />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
 
     <Footer/>
