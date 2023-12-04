4bc2bca8dd49b94d540ce82ea70fee887df21687
TechSergio <smarquez15@gmail.com>
2023-12-03 21:05:40 -0300

    Comprobar si la fecha seleccionada es valida

--- src/Components/Calendar.jsx
+++ src/Components/Calendar.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 
-
 const Calendar = ({ unavailablePeriods, onDatesSelected }) => {
   const [selectedRange, setSelectedRange] = useState({ from: null, to: null });
 
@@ -11,15 +10,31 @@ const Calendar = ({ unavailablePeriods, onDatesSelected }) => {
     onDatesSelected(range.from, range.to);
   };
 
-  const isDayUnavailable = (day) => {
-    const unavailablePeriod = unavailablePeriods.find((period) =>
-      day >= new Date(period.fechaInicio) && day <= new Date(period.fechaFin)
-    );
-    return unavailablePeriod ? true : false;
+  const handleReservation = () => {
+    if (selectedRange.from && selectedRange.to) {
+      let isValid = true;
+  
+      unavailablePeriods.forEach((periodo) => {
+        const fechaInicio = new Date(periodo.fechaInicio);
+        const fechaFin = new Date(periodo.fechaFin);
+  
+        if (
+          fechaInicio >= selectedRange.from && fechaInicio <= selectedRange.to ||
+          fechaFin >= selectedRange.from && fechaFin <= selectedRange.to
+        ) {
+          isValid = false;
+        }
+      });
+  
+      if (isValid) {
+        console.log('Reservado!!');
+      } else {
+        console.log('La fecha no es válida.');
+      }
+    } else {
+      console.log('Por favor, selecciona un rango de fechas.');
+    }
   };
-  
-  
-
 
   return (
     <div>
@@ -27,12 +42,16 @@ const Calendar = ({ unavailablePeriods, onDatesSelected }) => {
         mode="range"
         selected={selectedRange}
         onSelect={handleSelect}
-      disabled={isDayUnavailable}
-              numberOfMonths={2} // Muestra dos meses en el calendario para permitir la selección de un rango
+        disabled={(day) => unavailablePeriods.some(period => {
+          const fechaInicio = new Date(period.fechaInicio);
+          const fechaFin = new Date(period.fechaFin);
+          return day >= fechaInicio && day <= fechaFin;
+        })}
+        numberOfMonths={2}
       />
+      <button onClick={handleReservation}>Reservar</button>
     </div>
   );
 };
 
 export default Calendar;
-
