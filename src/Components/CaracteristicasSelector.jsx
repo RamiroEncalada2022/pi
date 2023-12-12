e256e5ad74258b7871827c7991638e1e8b357f79
TechSergio <smarquez15@gmail.com>
2023-12-12 15:25:41 -0300

    Repara funcionalidades, y envia a el backend

--- src/Components/CaracteristicasSelector.jsx
+++ src/Components/CaracteristicasSelector.jsx
@@ -1,46 +1,41 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './Styles/CaracteristicasSelector.module.css';
 import { useContextGlobal } from './utils/global.context';
 
 const CaracteristicasSelector = ({ onChange }) => {
-    const { state } = useContextGlobal();
+  const { state } = useContextGlobal();
+  const [selectedCaracteristicas, setSelectedCaracteristicas] = useState([]);
 
-    const [selectedCaracteristicas, setSelectedCaracteristicas] = useState([]);
-  
-    const handleCaracteristicaChange = (e) => {
-      const { value } = e.target;
-      const caracteristicaId = parseInt(value, 10);
-  
-      const updatedCaracteristicas = selectedCaracteristicas.includes(caracteristicaId)
-        ? selectedCaracteristicas.filter((id) => id !== caracteristicaId)
-        : [...selectedCaracteristicas, caracteristicaId];
-  
-      setSelectedCaracteristicas(updatedCaracteristicas);
-    };
-  
-    useEffect(() => {
-      console.log("Características en el estado:", state.caracteristicas);
-      console.log("Características seleccionadas:", selectedCaracteristicas);
-    }, [state.caracteristicas, selectedCaracteristicas]);
+  const handleCaracteristicaChange = (e) => {
+    const { value } = e.target;
+    const caracteristicaId = parseInt(value, 10);
 
-    return (
-      <div className={styles.caracteristicasContainer}>
-        <h4 className={styles.title}>Selecciona las características:</h4>
-        {state.caracteristicas.map((caracteristica) => (
-          <div key={caracteristica.id} className={styles.caracteristicaItem}>
-            <label>
-              <input
-                type="checkbox"
-                value={caracteristica.id}
-                checked={selectedCaracteristicas.includes(caracteristica.id)}
-                onChange={handleCaracteristicaChange}
-              />
-              {caracteristica.nombre}
-            </label>
-          </div>
-        ))}
-      </div>
-    );
+    const updatedCaracteristicas = selectedCaracteristicas.includes(caracteristicaId)
+      ? selectedCaracteristicas.filter((id) => id !== caracteristicaId)
+      : [...selectedCaracteristicas, caracteristicaId];
+
+    setSelectedCaracteristicas(updatedCaracteristicas);
+    onChange(updatedCaracteristicas);
+  };
+
+  return (
+    <div className={styles.caracteristicasContainer}>
+      <h4 className={styles.title}>Selecciona las características:</h4>
+      {state.caracteristicas.map((caracteristica) => (
+        <div key={caracteristica.id} className={styles.caracteristicaItem}>
+          <label>
+            <input
+              type="checkbox"
+              value={caracteristica.id}
+              checked={selectedCaracteristicas.includes(caracteristica.id)}
+              onChange={handleCaracteristicaChange}
+            />
+            {caracteristica.nombre}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CaracteristicasSelector;
